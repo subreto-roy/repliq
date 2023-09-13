ea3d8f6ac77d759d2ea99279e50694b14c2116f6
subreto roy <sroy172182@bscse.uiu.ac.bd>
2023-09-13 23:56:31 +0600

    add serializer

--- asset_tracking_app/serializers.py
+++ asset_tracking_app/serializers.py
@@ -7,3 +7,20 @@ class CompanySerializer(serializers.ModelSerializer):
         model = Company
         fields = '__all__'
 
+# Serializer for the Employee model
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = '__all__'
+
+# Serializer for the Device model
+class DeviceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Device
+        fields = '__all__'
+
+# Serializer for the DeviceLog model
+class DeviceLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DeviceLog
+        fields = '__all__'
